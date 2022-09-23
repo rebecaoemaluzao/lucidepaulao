52db626f3601653f4e45246606ff17a0331e5428
rebecaoemaluzao <114233381+rebecaoemaluzao@users.noreply.github.com>
2022-09-23 09:40:29 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+let p2 = 0
+let p1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("let's play")
+    basic.pause(randint(1000, 10000))
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    p2 = 1
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    p1 += 1
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
